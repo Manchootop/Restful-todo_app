457756ba1711a5faadfb51b7565d104d6aee036b
Mariyan Georgiev <mariqn5000@gmail.com>
2022-10-14 23:29:29 +0300

    registered todo models

--- Todo_drf/api/admin.py
+++ Todo_drf/api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from Todo_drf.api.models import Task
+from Todo_drf.api.models import Task, Category
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user__username', 'state')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
