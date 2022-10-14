039fc273744dbd889ea4c2e709ea845f0e96cd8e
Mariyan Georgiev <mariqn5000@gmail.com>
2022-10-14 23:27:32 +0300

    added todo models

--- Todo_drf/api/models.py
+++ Todo_drf/api/models.py
@@ -1,11 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 
+UserModel = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=15,
+    )
+
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    complete = models.BooleanField(default=False, blank=True, null=True)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    state = models.BooleanField(default=False, blank=True, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
